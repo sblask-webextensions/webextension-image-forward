bbab3bea0f9436b6502ea7e3de87fcd86a96820d
Sebastian Blask <blask@gmx.de>
2014-01-08 19:05:48 +0100

    Add necessary files

---
+++ content/imageforward.js
@@ -0,0 +1,18 @@
+var gImageForward = {
+
+    onLoad: function() {
+	    if ('undefined' == typeof gBrowser) {
+	        return;
+	    }
+	    window.removeEventListener('load', gImageForward.onLoad, false);
+	    window.addEventListener('unload', gImageForward.onUnload, false);
+    },
+
+    onUnload: function() {
+	    window.removeEventListener('unload', gImageForward.onUnload, false);
+    },
+
+};
+
+window.addEventListener('load', gImageForward.onLoad, false);
+
