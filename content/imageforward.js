1ec342621d978813690eb625628cf3aa343203a5
Sebastian Blask <blask@gmx.de>
2014-01-17 09:07:38 +0100

    Get links from document

--- content/imageforward.js
+++ content/imageforward.js
@@ -18,17 +18,49 @@ var gImageForward = {
     },
 
     onClick: function() {
-        var urls = new Array();
-        urls[0] = "http://www.test.com/";
-        urls[1] = "http://www.test.de/";
-        urls[2] = "http://www.test.com/";
-        gImageForward.addURLsToHistoryAndAdvance(urls);
+        var documentURLs = gImageForward.getURLs(gImageForward.getDocuments());
+        for(var index = 0; index < documentURLs.length; index++) {
+            var urlsAndReferrer = documentURLs[index];
+            gImageForward.addURLsToHistoryAndAdvance(urlsAndReferrer[0], urlsAndReferrer[1]);
+        }
     },
 
-    addURLsToHistoryAndAdvance: function(urls) {
+    getDocuments: function() {
+        var documents = new Array();
+        documents.push(gBrowser.selectedBrowser.contentWindow.document);
+        for(var index = 0; index < gBrowser.selectedBrowser.contentWindow.frames.length; index++) {
+            documents.push(gBrowser.selectedBrowser.contentWindow.frames[index].document);
+        }
+        return documents;
+    },
+
+    getURLs: function(documents) {
+        var result = new Array()
+        for(var index = 0; index < documents.length; index++) {
+            var tuple = new Array();
+            tuple.push(gImageForward.matchURLs(documents[index].links));
+            tuple.push(documents[index].URL);
+            result.push(tuple);
+        }
+        return result;
+    },
+
+    matchURLs: function(urls) {
+        var result = new Array();
+        var regexp = new RegExp("[^\?]+\.jpg$", "i");
+        for(var index = 0; index < urls.length; index++) {
+            var urlString = urls[index].toString();
+            if (urlString.match(regexp)) {
+                result.push(urls[index].toString());
+            }
+        }
+        return result;
+    },
+
+    addURLsToHistoryAndAdvance: function(urls, referrer) {
         for (var index = 0; index < urls.length; index++) {
             var url = urls[index];
-            gImageForward.addHistoryEntry(url, url, "http://www.nowhere.com");
+            gImageForward.addHistoryEntry("" + index, url, referrer);
         }
         // have to go back actually, because the history index is changed by
         // adding an entry while not loading it
