807acf5a397e4d0a50940a4e787bbebfbecfe2f8
Sebastian Blask <blask@gmx.de>
2014-03-08 20:49:23 +0100

    Make extension restartless

---
+++ bootstrap.js
@@ -0,0 +1,70 @@
+Components.utils.import("resource://gre/modules/devtools/Console.jsm");
+Components.utils.import("resource://gre/modules/Services.jsm");
+
+var initFunction = function(domWindow) {
+    var extensionResource = "chrome://imageforward/content/imageforward.jsm";
+    Components.utils.import(extensionResource);
+    domWindow.gImageForward = new ImageForward();
+    domWindow.gImageForward.initialize(domWindow);
+};
+
+var destroyFunction = function(domWindow) {
+    domWindow.gImageForward.destroy();
+    domWindow.gImageForward = undefined;
+};
+
+var setDefaultPreferences = function() {
+    var branch = Services.prefs.getDefaultBranch("extensions.imageforward.");
+    branch.setCharPref("imageURLRegExp", "^.*$");
+    branch.setCharPref("linkURLRegExp", "^[^#\\?]+\\.(jpg|jpeg|png)((#|\\?).*)?$");
+    branch.setIntPref("minHeight", 200);
+    branch.setIntPref("minWidth", 300);
+};
+
+function simpleToDomWindow(aWindow) {
+    return aWindow.QueryInterface(Components.interfaces.nsIDOMWindow);
+}
+
+function toDomWindow(aWindow) {
+    return aWindow
+              .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+              .getInterface(Components.interfaces.nsIDOMWindowInternal ||
+                  Components.interfaces.nsIDOMWindow);
+}
+
+var windowListener = {
+    onOpenWindow: function (aWindow) {
+        var domWindow = toDomWindow(aWindow);
+        var onLoadFunction = function() {
+            domWindow.removeEventListener("load", arguments.callee, false);
+            initFunction(domWindow);
+        };
+        domWindow.addEventListener("load", onLoadFunction, false);
+    },
+    onCloseWindow: function (aWindow) {},
+    onWindowTitleChange: function (aWindow, aTitle) {}
+};
+
+var windowMediator =
+    Components.classes["@mozilla.org/appshell/window-mediator;1"]
+        .getService(Components.interfaces.nsIWindowMediator);
+
+function callOnOpenWindows(someFunction) {
+    var openWindows = windowMediator.getEnumerator("navigator:browser");
+    while (openWindows.hasMoreElements()) {
+        someFunction(simpleToDomWindow(openWindows.getNext()));
+    }
+}
+
+function install() {}
+function uninstall() {}
+function startup(data, reason) {
+    setDefaultPreferences();
+    callOnOpenWindows(initFunction);
+    windowMediator.addListener(windowListener);
+}
+function shutdown(data, reason) {
+    windowMediator.removeListener(windowListener);
+    callOnOpenWindows(destroyFunction);
+}
+
--- defaults/preferences/imageforward_preferences.js
+++
@@ -1,5 +0,0 @@
-pref("extensions.imageforward.imageURLRegExp", "^[^#\?]+\.(jpg|jpeg|png)((#|\?).*)?$");
-pref("extensions.imageforward.linkURLRegExp", "^[^#\?]+\.(jpg|jpeg|png)((#|\?).*)?$");
-pref("extensions.imageforward.minHeight", 200);
-pref("extensions.imageforward.minWidth", 300);
-
